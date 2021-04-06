cdb9ad40e6434b102b5925567480b86c7c493713
SketchMaster2001 <75850871+SketchMaster2001@users.noreply.github.com>
2021-04-05 21:08:53 -0400

    Add support for OAuth2

--- card_site/routes.py
+++ card_site/routes.py
@@ -1,7 +1,34 @@
+from flask import render_template, redirect, url_for
+from flask_discord import DiscordOAuth2Session
 from main import app
-from flask import render_template
+
+discord = DiscordOAuth2Session(app)
 
 
 @app.route("/")
-def home():
-    return render_template("home.html")
+def index():
+    if not discord.authorized:
+        return render_template("home.html", user="e")
+
+    user = discord.fetch_user()
+
+    return render_template("home.html", user=user.avatar_url, username=user.name, user_id=user.id)
+
+
+@app.route("/login/")
+def login():
+    return discord.create_session()
+
+
+@app.route("/callback/")
+def callback():
+    data = discord.callback()
+    redirect_to = data.get("redirect", "/")
+    return redirect(redirect_to)
+
+
+@app.route("/logout/")
+def logout():
+    discord.revoke()
+    return redirect(url_for(".index"))
+
