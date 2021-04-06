df43d7460fffe0cf2b8395fe420b533add9242ec
SketchMaster2001 <75850871+SketchMaster2001@users.noreply.github.com>
2021-04-05 21:07:38 -0400

    Add support for OAuth2

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
+import os
+
 from flask import Flask
 
-import card_site
 
 app = Flask(__name__)
 
-if __name__ == '__main__':
+app.secret_key = b"%\xe0'\x01\xdeH\x8e\x85m|\xb3\xffCN\xc9g"
+os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "true"    # !! Only in development environment.
+
+app.config["DISCORD_CLIENT_ID"] = os.getenv("SECRET_ID")
+app.config["DISCORD_CLIENT_SECRET"] = os.getenv("SECRET_KEY")
+app.config["DISCORD_REDIRECT_URI"] = "http://localhost/callback"
+
+if __name__ == "__main__":
     app.run()
+
+import card_site
