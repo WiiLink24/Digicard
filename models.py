23d9180b80f0a9722315ec5722dbfc5b507d55f3
zurgeg <zurgeg@gmail.com>
2021-05-15 16:16:35 -0400

    Only allow business cards

---
+++ models.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+
+from main import db
+
+
+class Orders(db.Model):
+    order_id = db.Column(db.String(14), primary_key=True, unique=True)
+    date_created = db.Column(db.DateTime, default=datetime.now(), nullable=False)
+    is_business_card = db.Column(db.Boolean, default=False, nullable=False)
+    email = db.Column(db.String(127))
+    order_schema = db.Column(db.UnicodeText())
+
+
+class Images(db.Model):
+    image_id = db.Column(db.String(7), nullable=False, primary_key=True)
+    order_id = db.Column(
+        db.String(14), db.ForeignKey("orders.order_id"), nullable=False
+    )
+    filename = db.Column(db.String, nullable=False)
