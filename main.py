bd647365b3e8c7998d86281f591ca5e0b8e1369d
SketchMaster2001 <75850871+SketchMaster2001@users.noreply.github.com>
2021-05-15 22:01:02 -0400

    Define SQL traits before setting SQLAlchemy

--- main.py
+++ main.py
@@ -3,7 +3,12 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
+import config
+
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = config.db_url
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
 db = SQLAlchemy(app)
 
 app.secret_key = b"%\xe0'\x01\xdeH\x8e\x85m|\xb3\xffCN\xc9g"
@@ -11,8 +16,7 @@ os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "true"    # !! Only in development e
 
 app.config["DISCORD_CLIENT_ID"] = os.getenv("SECRET_ID")
 app.config["DISCORD_CLIENT_SECRET"] = os.getenv("SECRET_KEY")
-app.config["DISCORD_REDIRECT_URI"] = "http://card.wiilink24.com/callback"
-app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("SQLALCHEMY_DATABASE_URI", 'sqlite:///amongus.db')
+app.config["DISCORD_REDIRECT_URI"] = "http://dev.wiilink24.com"
 if __name__ == "__main__":
     app.run()
 
