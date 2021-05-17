eaa2221659442d29e73bddc6f4fd83853537c0a4
Spotlight <spotlight@joscomputing.space>
2021-05-16 19:20:39 -0500

    Allow insecure oauth transport

--- main.py
+++ main.py
@@ -12,9 +12,8 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
 app.secret_key = config.secret_key
-if app.debug:
-    # !! Only use in a development environment.
-    os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "true"
+# !! Only set due to proxy_pass from Nginx.
+os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "true"
 
 app.config["DISCORD_CLIENT_ID"] = config.discord_client_id
 app.config["DISCORD_CLIENT_SECRET"] = config.discord_client_secret
